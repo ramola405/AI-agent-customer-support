228b0a779400b9adc310edf422228d0c0c805c12
ramola405 <45432545+ramola405@users.noreply.github.com>
2024-10-19 17:57:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name="self-operating-computer",
+    name="AI-agent-customer-support",
     version="1.4.6",
     packages=find_packages(),
     install_requires=required,  # Add dependencies here
